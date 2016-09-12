6f0f3bdd283348b0526ef163fbca7cc65d21235f
unknown <vivien.adnot@gmail.com>
2016-09-12 18:33:50 +0200

    moved reward to tagproviders

---
+++ app/tagProviders/reward/Reward.ctor.js
@@ -0,0 +1,19 @@
+playtemEmbedded.Reward = function(options) {
+    var defaults = {
+        apiKey: undefined,
+        gameType: undefined
+    };
+
+    this.settings = {
+        scriptUrl: "//api.playtem.com/advertising/services.reward/",
+        sendEvents: {
+            userId: "playtem:tagApp:userId",
+        }
+    };
+
+    this.userId = null;
+    this.executeCallback = null;
+
+    this.defaults = $.extend(defaults, options);
+    this.settings = $.extend(this.settings, defaults);       
+};
---
+++ app/tagProviders/reward/userIdMessageHandler.js
@@ -0,0 +1,22 @@
+playtemEmbedded.Reward.prototype.userIdMessageHandler = function(postMessage) {
+    var self = playtemEmbedded.Core.globals.playtemRewardContext;
+    var playtemIdentifier = "playtem:js:";
+
+    if(!postMessage || !postMessage.data) {
+        return;
+    }
+
+    var userIdMessage = postMessage.data;
+
+    if(userIdMessage.indexOf(playtemIdentifier) != 0) {
+        return;
+    }
+
+    var extractUserId = function() {
+        return userIdMessage.substring(playtemIdentifier.length);
+    };
+
+    self.userId = extractUserId();
+
+    self.getReward(playtemEmbedded.Core.Operations.noop);
+};
