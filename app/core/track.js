5ac3ce1ed2aaa16ea8ff36c86b182c2001a0ffe6
unknown <vivien.adnot@gmail.com>
2016-11-14 18:34:18 +0100

    updated tracker log to add more informations

--- app/core/track.js
+++ app/core/track.js
@@ -7,8 +7,12 @@ playtemEmbedded.Core.track = function(providerName, apiKey, eventType, callback)
     var url = "//api.playtem.com/tracker.gif?a=" + eventType + "&c=&p=" + providerName + "&k=" + apiKey + "&t=" + timestamp;
 
     $.get(url)
-        .fail(function() {
-            playtemEmbedded.Core.log("playtemEmbedded", "couldn't retrieve pixel tracking from: " + url);
+        .fail(function(jqxhr) {
+            if(typeof jqxhr == "object" && jqxhr.status && jqxhr.statusText) {
+                playtemEmbedded.Core.log("playtemEmbedded", "pixel tracking fail. " + jqxhr.status + ": " + jqxhr.statusText);
+            } else {
+                playtemEmbedded.Core.log("playtemEmbedded", "pixel tracking fail. UNKNOWN");
+            }
         })
         .always(function() {
             callback();
