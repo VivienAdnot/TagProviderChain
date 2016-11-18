ef4e51d0c1f771ef0308ca5ea9f6b19402f3987a
unknown <vivien.adnot@gmail.com>
2016-11-18 16:07:57 +0100

    updated detail

--- app/tagProviders/placementProfileBehaviour/getPlacementRewardedBehavior.js
+++ app/tagProviders/placementProfileBehaviour/getPlacementRewardedBehavior.js
@@ -2,21 +2,21 @@ playtemEmbedded.TagProviders.prototype.getPlacementRewardedBehavior = function (
     var self = this;
 
     var adCompleteOrError = function() {
-        if(self.settings.hasReward == true) {
-            var rewarder = new playtemEmbedded.Reward({
-                apiKey: self.settings.apiKey
-            });
-
-            rewarder.execute()
-            .fail(function(error) {
-                playtemEmbedded.Core.log("reward", error);
-            })
-            .always(function() {
-                self.windowBlocker.clearBlocker();
-            });
-        } else {
+        if(self.settings.hasReward == false) {
             self.windowBlocker.clearBlocker();
         }
+
+        var rewarder = new playtemEmbedded.Reward({
+            apiKey: self.settings.apiKey
+        });
+
+        rewarder.execute()
+        .fail(function(error) {
+            playtemEmbedded.Core.log("reward", error);
+        })
+        .always(function() {
+            self.windowBlocker.clearBlocker();
+        });
     };
 
     return {
