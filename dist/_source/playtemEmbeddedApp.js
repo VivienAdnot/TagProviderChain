b32be5171a421c70b83f9bf968011d00893d4de0
unknown <vivien.adnot@gmail.com>
2016-11-23 18:12:31 +0100

    temp commit

--- dist/_source/playtemEmbeddedApp.js
+++ dist/_source/playtemEmbeddedApp.js
@@ -1610,6 +1610,53 @@ playtemEmbedded.PlaytemVastInstream.prototype.execute = function() {
     self.vastPlayer.execute();
 };
 
+playtemEmbedded.PlaytemVastInstreamSpotx = function(options) {
+    var defaults = {
+        debug: false,
+        apiKey: undefined,
+
+        onAdAvailable: $.noop,
+        onAdUnavailable: $.noop,
+        onAdComplete: $.noop,
+        onAdError: $.noop
+    };
+
+    this.settings = {
+
+    };
+
+    this.vastPlayer = undefined;
+
+    this.defaults = $.extend(defaults, options);
+    this.settings = $.extend(this.settings, defaults);
+
+    if(this.settings.debug === true) {
+        // nothing to do
+    }
+};
+
+playtemEmbedded.PlaytemVastInstreamSpotx.prototype.execute = function() {
+    var self = this;
+
+    var buildTag = function() {
+        return "../vast/rewarded/spotx.xml?" + playtemEmbedded.Core.Date.getCurrentTimestamp();
+    };
+
+    self.vastPlayer = new playtemEmbedded.PlaytemVastPlayer({
+        debug: self.settings.debug,
+        vastTag: buildTag(),
+        apiKey: self.settings.apiKey,
+        providerName: "PlaytemVastInstreamSpotx",
+
+        onAdAvailable: self.settings.onAdAvailable,
+        onAdUnavailable: self.settings.onAdUnavailable,
+        onAdComplete: self.settings.onAdComplete,
+        onAdError: self.settings.onAdError
+    });
+
+    self.vastPlayer.execute();
+};
+
 playtemEmbedded.PlaytemVastOutstream = function(options) {
     var defaults = {
         debug: false,
