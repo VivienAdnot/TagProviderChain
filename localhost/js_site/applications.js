ba0e2c1179b053447c350f2607e9d72e21aab778
unknown <vivien.adnot@gmail.com>
2016-11-23 16:15:12 +0100

    renamed test to localhost (more explicit)

---
+++ localhost/js_site/applications.js
@@ -0,0 +1,23 @@
+var listApikey = {
+    "Ludokado rewarded": "4a2b-8438v",
+    "Belote rewarded": "4da9-acb2b",
+    "IsCool rewarded" : "452c-8a80i",
+    "UrbanRivals rewarded": "494f-8f1bv",
+    "Jotu rewarded": "4c3f-be78j",
+    "LadyPopular rewarded": "r47c7-9c9b",
+    "LadyPopular rewarded test": "TSTr7-9c9b",
+    "Mediastay rewarded": "r4b75-8a76",
+    "Mediastay rewarded test": "TSTr5-8a76",
+    
+    "Jotu outstream": "1c27-4684v",
+    "Ludokado outstream": "9a19-43fav",
+    "LadyPopular outstream": "92c6-497ff",
+    "Mediastay outstream": "e048-4cdev",
+    "ActiplayNoReward outstream": "4f63-b20df",
+    "MaBimbo outstream": "1206-4ce9f",
+    "Voyage to Fantasy outstream": "9a56-4422f",
+    "Equideow outstream": "ff3d-4b8cf",
+    "Prizee outstream": "Fj68VbKzEe",
+    "FairyMix outstream": "Hn78Uf8iRy",
+    "Test": "XXXX-xxxxx"
+};
