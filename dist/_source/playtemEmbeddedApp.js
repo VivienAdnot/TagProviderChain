7132dbd84437498438905282033a17686796cee0
unknown <vivien.adnot@gmail.com>
2016-11-24 10:59:47 +0100

    bugfix reward when loop on userIdMessageHandler

--- dist/_source/playtemEmbeddedApp.js
+++ dist/_source/playtemEmbeddedApp.js
@@ -332,23 +332,20 @@ playtemEmbedded.Reward.prototype.userIdMessageHandler = function(postMessage) {
     var self = playtemEmbedded.Core.globals.playtemRewardContext;
     var playtemIdentifier = "playtem:js:";
 
-    if(!postMessage || !postMessage.data) {
+    try {
+        var userIdMessage = postMessage.data;
+
+        if(userIdMessage.indexOf(playtemIdentifier) == 0) {
+            self.userId = userIdMessage.substring(playtemIdentifier.length);
+            self.getReward(self.executeCallback);
+
+            window.removeEventListener("message", self.userIdMessageHandler, false);
+        }
+    }
+    
+    catch(e) {
         return;
     }
-
-    var userIdMessage = postMessage.data;
-
-    if(userIdMessage.indexOf(playtemIdentifier) != 0) {
-        return;
-    }
-
-    var extractUserId = function() {
-        return userIdMessage.substring(playtemIdentifier.length);
-    };
-
-    self.userId = extractUserId();
-
-    self.getReward(self.executeCallback);
 };
 
 playtemEmbedded.TagProviders = function (options) {
